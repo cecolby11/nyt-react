eea7b7fcb29ec967f01766a50b32782202e836b8
cecolby11 <cecolby11@users.noreply.github.com>
2017-06-21 12:17:49 -0500

    hitting NYT api

--- models/Article.js
+++ models/Article.js
@@ -1,11 +1,33 @@
-var Article = new Schema({
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
   title: {
-
+    type: String,
+    trim: true,
+    unique: true,
+    required: true, 
   }, 
+  author: {
+    type: String,
+    trim: true,
+    required: false
+  },
+  snippet: {
+    type: String,
+    trim: true,
+    required: false
+  },
   date: {
-
+    type: Date
   }, 
   url: {
-
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
   }
 });
+
+var Article = mongoose.model('Article', ArticleSchema);
+module.exports = Article;
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var request = require('request');
 
-var Click = require('./models/Article.js');
+var Article = require('./models/Article.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -30,6 +31,43 @@ db.once('open', function() {
 });
 
 // routes
+app.get('/api/query', function(req, res) {
+  var apiKey = 'b0a8918d8a0742a5882fc8da181a0921';
+  var queryTerm = 'trump';
+  // searches for YYYYMMDD
+  var beginDate = 2016 + '0101';
+  var endDate = 2017 + '1231';
+  var searchUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + apiKey + '&q=' + queryTerm + '&begin_date=' + beginDate + '&end_date=' + endDate;
+  request(searchUrl, function(error, response, body) {
+    if(error) {
+      console.log(error);
+    } else {
+      var results = JSON.parse(body).response.docs;
+      var articles = [];
+      // go through results and pull out relevant info
+      for(var i = 0; i < results.length; i++) {
+        var article = {
+          title: results[i].headline.main,
+          author: results[i].news_desk,
+          date: results[i].pub_date,
+          url: results[i].web_url,
+          snippet: results[i].snippet
+        }
+        articles.push(article);
+        // add to database
+        var newArticle = new Article(article);
+        newArticle.save(function(err, doc) {
+          if(err) {
+            res.send(err);
+          } else {
+            console.log('new article added to db!');
+          }
+        });
+      }
+      res.send(articles);
+    }
+  })
+})
 // GET: for querying mongodb
 app.get('/api/saved', function(req, res) {
   // TODO
@@ -50,5 +88,9 @@ app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.listen(PORT, function() {
+  console.log('App listening');
+});
 
-
+// TODO
+// remove API key from NYT query 
